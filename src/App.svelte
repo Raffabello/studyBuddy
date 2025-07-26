98ee94f908224042d7a00d1c609844c081c4370a
Raffaele <that.raffaele95bin@gmail.com>
2025-07-26 19:16:04 +0900

    Added main content to the app

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,18 @@
 </script>
 
 <main>
-
+  <div id="display">
+    <div id="card-manager">
+      <div id="card-manager-header">
+        <input type="text" placeholder="Think a word ..."/>
+        <input type="button" value="Enter"/>
+      </div>
+      <div id="card-manager-body">
+        
+      </div>
+    </div>
+  </div>
 </main>
 
 <style>
-
 </style>
